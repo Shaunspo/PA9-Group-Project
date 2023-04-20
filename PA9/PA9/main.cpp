203819fd782cf58814f7c456724d50a2cdd0e19c
Shaunspo <71531207+Shaunspo@users.noreply.github.com>
2023-04-20 12:52:23 -0700

    Update main.cpp

---
+++ PA9/PA9/main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::fstream;
+using std::ifstream;
+using std::ostream;
+
+
+int main(void) {
+
+
+	std::cout << "Hello World!" << endl;
+
+	return 0;
+
+}
