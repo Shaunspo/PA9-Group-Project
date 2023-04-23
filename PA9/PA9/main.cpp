1d41a1c9bb467d89847e400189c75bf76241f461
jjmoosman <72100434+jjmoosman@users.noreply.github.com>
2023-04-22 17:05:45 -0700

    Major Update Hopefully it works

---
+++ PA9/PA9/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+
+using sf::RenderWindow;
+using sf::CircleShape;
+using sf::Color;
+using sf::VideoMode;
+using sf::Event;
+
+
+int main()
+{
+	RenderWindow window(VideoMode(1920, 1080), "Test Render");
+	CircleShape testshape(100.f);
+	testshape.setFillColor(Color::Blue);
+
+	while (window.isOpen())
+	{
+		Event testEvent;
+		while (window.pollEvent(testEvent))
+		{
+			if (testEvent.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(testshape);
+		window.display();
+
+	}
+
+
+
+	return 0;
+}
