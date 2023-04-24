348a2ac72827dbbe96652dc07e75b6d9ed17173c
seth-aguilar <sethcaguilar@gmail.com>
2023-04-23 19:08:52 -0700

    Gravity!
    added gravity to the entity class. no collision yet so it kinda just falls

--- PA9/PA9/Entity.cpp
+++ PA9/PA9/Entity.cpp
@@ -22,39 +22,81 @@ void Entity::initSprite()
 	this->sprite.setTexture(testTexture);
 }
 
+void Entity::initPhysics()
+{
+	this->velocityMax = 50.f;
+	this->velocityMin = 1.f;
+	this->acceleration = 3.f;
+	this->drag = 0.93f;
+	this->gravity = 4.f;
+	this->velocityMaxY = 15.f;
+}
+
 Entity::Entity()
 {
 	this->initTexture();
 	this->initSprite();
+	this->initPhysics();
 }
 
 Entity::~Entity()
 {
 }
 
+void Entity::move(const float dir_x, const float dir_y)
+{
+	//Accelerration
+	this->velocity.x += dir_x * this->acceleration;
+
+	//Limit velocity
+	if (std::abs(this->velocity.x) > this->velocityMax)
+	{
+		this->velocity.x = velocityMax * ((this->velocity.x < 0) ? -1.f : 1.f);
+	}
+}
+
+void Entity::updatePhysics()
+{
+	//Gravity
+	this->velocity.y += 1.0 * this->gravity;
+	if (std::abs(this->velocity.y) > this->velocityMaxY)
+	{
+		this->velocity.y = velocityMaxY * ((this->velocity.y < 0) ? -1.f : 1.f);
+	}
+
+
+	//Deceleration
+	this->velocity *= this->drag;
+
+	//Limit deceleration
+	if (std::abs(this->velocity.x) < this->velocityMin)
+	{
+		this->velocity.x = 0.f;
+	}
+	if (std::abs(this->velocity.y) < this->velocityMin)
+	{
+		this->velocity.y = 0.f;
+	}
+
+	this->sprite.move(this->velocity);
+}
+
 void Entity::updateMovement()
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) //Left
 	{
-		this->sprite.move(-1.f, 0.f);
+		this->move(-0.5f, 0.f);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) //Right
 	{
-		this->sprite.move(1.f, 0.f);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) //Up
-	{
-		this->sprite.move(0.f, -1.f);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) //Down
-	{
-		this->sprite.move(0.f, 1.f);
+		this->move(0.5f, 0.f);
 	}
 }
 
 void Entity::update()
 {
 	this->updateMovement();
+	this->updatePhysics();
 }
 
 void Entity::render(sf::RenderTarget& target)
--- PA9/PA9/Entity.h
+++ PA9/PA9/Entity.h
@@ -15,17 +15,27 @@ private:
 
 	//Movement
 
+	//Physics
+	sf::Vector2f velocity;
+	float velocityMax;
+	float velocityMin;
+	float acceleration;
+	float drag;
+	float gravity;
+	float velocityMaxY;
+
 	//Core
-
-
 	void initTexture();
 	void initSprite();
+	void initPhysics();
 
 public:
 	Entity();
 	~Entity();
 
 	//Functions
+	void move(const float dir_x, const float dir_y);
+	void updatePhysics();
 	void updateMovement();
 	void update();
 	void render(sf::RenderTarget& target);
